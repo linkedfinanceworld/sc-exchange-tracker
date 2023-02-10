097d4824b3189c339935877ebbf2d35b781896a9
noisynoah <noisy.noah910@gmail.com>
2023-02-10 10:22:05 +0700

    main SC

---
+++ contracts/KlardaExchangeTracker_CRO.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract KlardaExchangeTracker_CRO {
+
+    function getExchangeBalance(
+        address[] memory _userAddress, 
+        address _tokenAddress
+    ) public view returns(uint256 _amount) {
+        for (uint i = 0; i <_userAddress.length; i++) {
+            _amount += IERC20(_tokenAddress).balanceOf(_userAddress[i]);
+        }
+    } 
+}
---
+++ hardhat.config.ts
@@ -0,0 +1,79 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+dotenv.config();
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.7",
+  networks: {
+    bsc_mainnet: {
+      url: process.env.BSC_MAINNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    eth_mainnet: {
+        url: process.env.ETHEREUM_MAINNET_URL || "",
+        accounts:
+          process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    polygon_mainnet: {
+      url: process.env.POLYGON_MAINNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    cronos: {
+      url: process.env.CRONOS_MAINNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    fantom_mainnet: {
+      url: process.env.FANTOM_MAINNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    avalanche_mainnet: {
+      url: process.env.AVALANCHE_MAINNET_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    //apiKey: process.env.BSCSCAN_API_KEY,
+    //apiKey: process.env.ETHERSCAN_API_KEY,
+    //apiKey: process.env.POLYGON_API_KEY,
+    //apiKey: process.env.AVALANCHE_API_KEY,
+    //apiKey: process.env.OPERA_API_KEY,
+    apiKey: process.env.CRONOSCAN_API_KEY,
+    customChains: [
+        {
+            network: "cronos",
+            chainId: 25,
+            urls: {
+                apiURL: "https://api.cronoscan.com/api",
+                browserURL: "https://cronoscan.com/"
+            },
+        },
+    ],
+  },
+};
+
+export default config;
